6f756edcf7a22935ce885f67e0caa7afd9a36301
Dmitry Petrochenkov <demon-st2002@yandex.ru>
2017-01-29 21:02:37 +0300

    guessing-game: guessing-game.js - problem "number is not defined"; package.json - "auto start mocha"

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = null;
+    	this.max = null;
+    	this.guessNumber = null;
+    	let number = null;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	let guessNumber = (this.min + this.max)/2;
+    	guessNumber = Math.round(guessNumber*10)/10;
+    	console.log(guessNumber);
+    	while (guessNumber !== number){
+    		if(guessNumber < number){
+    			this.lower();
+    		} else if (guessNumber > number){
+    			this.greater();
+    		};
+    	}
+    	return guessNumber;
     }
 
     lower() {
-
+    	this.min = guessNumber;
+    	this.max = max;
     }
 
     greater() {
-
+    	this.max = guessNumber;
+    	this.min = min;
     }
 }
 
